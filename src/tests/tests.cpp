d57941945b804f1c9e52c16d6e7ac4687162a3db
Nosachev Anton <nosachev.aa@phystech.edu>
2023-11-13 17:14:00 +0300

    Added all tests

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -2,28 +2,36 @@
 #include "../grammar.h"
 #include "../Earley.h"
 
+struct inputGrammar {
+    std::string buffer;
+    char startingSymbol;
+    std::unordered_set<char> nonTerminals;
+    std::unordered_set<char> terminals;
+    std::unordered_map<char, std::list<std::string>> rules;
+};
+
+const int countGrammars = 4;
+
+inputGrammar testingInputs[countGrammars] = {
+        {"1 2 2\nS\nab\nS->aSbS\nS->\nS"
+                ,'S', {'S'}, {'a', 'b'}
+                , {{'S', {"aSbS", ""}}} }
+        , {"1 2 2\nC\nab\nC->aCbC\nC->\nC"
+                ,'C', {'C'}, {'a', 'b'}
+                , {{'C', {"aCbC", ""}}} }
+        , {"1 2 2\nS\nab\nS->aSb\nS->\nS"
+                , 'S', {'S'}, {'a', 'b'}
+                , {{'S', {"aSb", ""}}} }
+        , {"3 6 6\nSAB\nabcdkz\nS->aSb\nS->AB\nA->c\nA->ddAk\nA->\nB->zzz\nS"
+                , 'S', {'S', 'A', 'B'}, {'a', 'b', 'c', 'd', 'k', 'z'}
+                , {{'S', {"aSb", "AB"}}, {'A', {"c", "ddAk", ""}}, {'B', {"zzz"}}}}
+};
+
 TEST(GrammarTests, Scan) {
-    struct inputScan {
-        std::string buffer;
-        char startingSymbol;
-        std::unordered_set<char> nonTerminals;
-        std::unordered_set<char> terminals;
-        std::unordered_map<char, std::list<std::string>> rules;
-    };
-
-    inputScan testingInputs[2] = {
-            {"1 2 2\nS\nab\nS->aSbS\nS->\nS"
-                    ,'S', {'S'}, {'a', 'b'}
-                    , {{'S', {"aSbS", ""}}} },
-            {"1 2 2\nC\nab\nC->aCbC\nC->\nC"
-                    ,'C', {'C'}, {'a', 'b'}
-                    , {{'C', {"aCbC", ""}}} }
-    };
-
     std::stringstream buffer;
     std::streambuf *sBuffer = std::cin.rdbuf();
 
-    auto testInput = [](const inputScan& input, std::stringstream& buffer){
+    auto testInput = [](const inputGrammar& input, std::stringstream& buffer){
         buffer.clear();
         buffer << input.buffer;
         std::cin.rdbuf(buffer.rdbuf());
@@ -46,9 +54,11 @@ TEST(GrammarTests, ScanExceptions) {
     std::stringstream buffer;
     std::streambuf *sBuffer = std::cin.rdbuf();
 
-    std::string buffers[3] = {"1 2 2\nS\nab\nS->ac\nS->\nS"
-    , "1 2 2\nS\nab\nS->ab\nS->\nB"
-    , "1 2 2\nS\nab\nS->ac\nS\nS"};
+    std::string buffers[5] = {"1 2 2\nS\nab\nS->aScS\nS->\nS"
+    , "1 2 2\nS\nab\nS->aSbS\nS->\nB"
+    , "1 2 2\nS\nab\nS->aSbS\nS\nS"
+    , "1 2 2\nS\nab\nS->aSbS\nS->B\nS"
+    , "1 2 2\nS\nab\nS->aSbS\nS->(\nS"};
 
     auto testException = [](const std::string& input, std::stringstream& buffer){
         buffer.clear();
@@ -63,4 +73,43 @@ TEST(GrammarTests, ScanExceptions) {
     }
 
     std::cin.rdbuf(sBuffer);
+}
+
+TEST(EarleyTests, Tests) {
+    std::vector<std::string> stringsInGrammar[countGrammars];
+    std::vector<std::string> stringsOutGrammar[countGrammars];
+
+    stringsInGrammar[0] = {"aababb", "aabb", ""};
+
+    stringsOutGrammar[0] = {"aabbba"};
+
+    stringsInGrammar[2] = {"", "ab", "aaaaaabbbbbb"};
+
+    stringsOutGrammar[2] = {"a", "aaaa", "c", "aab", "bbb", "9()9"};
+
+    stringsInGrammar[3] = {"zzz", "azzzb", "aazzzbb", "aaczzzbb", "czzz", "addddkkzzzb"};
+
+    stringsOutGrammar[3] = {"", "a", "aaaczzzbb", "c"};
+
+    for (int i = 0; i < countGrammars; ++i) {
+        std::stringstream buffer;
+        std::streambuf *sBuffer = std::cin.rdbuf();
+        buffer.clear();
+        buffer << testingInputs[i].buffer;
+        std::cin.rdbuf(buffer.rdbuf());
+
+        Grammar grammar;
+        grammar.scan();
+
+        Earley algo;
+        algo.fit(grammar);
+
+        for (const auto& string : stringsInGrammar[i]) {
+            EXPECT_EQ(true, algo.predict(string));
+        }
+
+        for (const auto& string : stringsOutGrammar[i]) {
+            EXPECT_EQ(false, algo.predict(string));
+        }
+    }
 }
